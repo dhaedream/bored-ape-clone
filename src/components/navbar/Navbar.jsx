b09c3684705e82c753c57a5dd5342a2af33bc94b
Dhaesha M <dhaesha1@gmail.com>
2022-03-14 16:26:28 -0400

    navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,44 +15,61 @@ const Navbar = () => {
       <div className="bayc__navbar-logo_container">
         <img src={logo} alt="logo" />
       </div>
-      <div className="bayc__navbar-links_container">
-        <p>
-          <a href="#buy-an-ape">BUY AN APE</a>
-        </p>
-        <p>
-          <a href="#roadmap">roadmap</a>
-        </p>
-        <p>
-          <a href="#team">team</a>
-        </p>
-        <p>
-          <a href="#gallery">gallery</a>
-        </p>
-        <p>
-          <a href="#provenance">provenance</a>
-        </p>
-        <p>
-          <a href="#members">members</a>
-        </p>
-      </div>
-      <div className="bayc__navbar-icons">
-        <a>
-          <LogoYoutube color={"#fff"} title={"yt"} height="25px" width="25px" />
-        </a>
-        <a>
-          <LogoInstagram
-            color={"#fff"}
-            title={"ig"}
-            height="25px"
-            width="25px"
-          />
-        </a>
-        <a>
-          <LogoDiscord color={"#fff"} title={"dc"} height="25px" width="25px" />
-        </a>
-        <a>
-          <LogoTwitter color={"#fff"} title={"tw"} height="25px" width="25px" />
-        </a>
+      <div className="bayc__navbar-outer-container">
+        <div className="bayc__navbar-links_container">
+          <p>
+            <a href="#buy-an-ape">BUY AN APE</a>
+          </p>
+          <p>
+            <a href="#roadmap">ROADMAP</a>
+          </p>
+          <p>
+            <a href="#team">TEAM</a>
+          </p>
+          <p>
+            <a href="#gallery">GALLERY</a>
+          </p>
+          <p>
+            <a href="#provenance">PROVENANCE</a>
+          </p>
+          <p>
+            <a href="#members">MEMBERS</a>
+          </p>
+        </div>
+        <div className="bayc__navbar-icons">
+          <a>
+            <LogoYoutube
+              color={"#fff"}
+              title={"yt"}
+              height="15px"
+              width="15px"
+            />
+          </a>
+          <a>
+            <LogoInstagram
+              color={"#fff"}
+              title={"ig"}
+              height="15px"
+              width="15px"
+            />
+          </a>
+          <a>
+            <LogoDiscord
+              color={"#fff"}
+              title={"dc"}
+              height="15px"
+              width="15px"
+            />
+          </a>
+          <a>
+            <LogoTwitter
+              color={"#fff"}
+              title={"tw"}
+              height="15px"
+              width="15px"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
