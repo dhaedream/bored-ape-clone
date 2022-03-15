359944d0b7c1bd5bc784935cbe4271a5cf6f6b92
Dhaesha M <dhaesha1@gmail.com>
2022-03-15 15:57:59 -0400

    scratching the idea of containers. Its taking me too long to short-hand that

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
-import Header from "./components/header/Header";
-import Welcome from "./containers/welcome/Welcome";
-
+// import Welcome from "./containers/welcome/Welcome";
+import { Navbar, Header } from "./components/index.js";
 export const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
-      <Welcome />
+      {/* <Welcome /> */}
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 // for expports
+export { default as Feature } from "./feature/Feature.jsx";
+export { default as Header } from "./header/Header.jsx";
+export { default as Navbar } from "./navbar/Navbar.jsx";
