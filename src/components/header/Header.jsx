b98e689d6185fdb01b28412f3efe58d89659b8d9
Dhaesha M <dhaesha1@gmail.com>
2022-03-15 02:38:26 -0400

    header img added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./header.css";
+import heroImg from "../../assets/bayc-mutant-hero.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bayc__header">
+      <div className="bayc__header-img_container">
+        <img src={heroImg} alt="header img" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
