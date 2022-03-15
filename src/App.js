eb58e59d39bb53eb3545c0efa08b7776348b82cc
Dhaesha M <dhaesha1@gmail.com>
2022-03-15 13:39:10 -0400

    working thru errors, Feature component broke the page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
+import Welcome from "./containers/welcome/Welcome";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Welcome />
     </div>
   );
 };
