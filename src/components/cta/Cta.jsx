7b638e3602c36312555c51ea814dcdcd3c44ad83
Dhaesha M <dhaesha1@gmail.com>
2022-03-16 11:59:54 -0400

    card shape + content getting put in place

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./cta.css";
 
 const Cta = () => {
-  return <div className="bayc__cta">Cta</div>;
+  return (
+    <div className="bayc__cta">
+      <div className="bayc__cta-card">
+        <h3>BUY AN APE</h3>
+        <p>
+          The initial sale has sold out. To get your Bored Ape, check out the
+          collection on OpenSea.
+        </p>
+        <button type="button">BUY AN APE ON OPENSEA</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
