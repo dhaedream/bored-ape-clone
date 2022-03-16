c836ef5b5f3847943315326c7c35b4b5b6b9707c
Dhaesha M <dhaesha1@gmail.com>
2022-03-16 00:05:12 -0400

    distribution component time! added all nodes. styling next

---
+++ src/components/distribution/Distribution.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./distribution.css";
+
+const Distribution = () => {
+  return (
+    <div>
+      <div className="bayc__distribution-container">
+        <div className="bayc__distribution-top">
+          <h2>FAIR DISTRIBUTION</h2>
+          <p>(BONDING CURVES ARE A PONZI)</p>
+        </div>
+        <div className="bayc__distribution-bottom">
+          <p className="bayc__distribution-bottom_left">
+            There are no bonding curves here. Buying a Bored Ape costs 0.08 ETH.
+            There are no price tiers; BAYC membership costs the same for
+            everyone.
+          </p>
+          <p className="bayc__distribution-bottom_right">
+            Note: Thirty apes are being withheld from the sale. These will be
+            used for giveaways, puzzle rewards—and for the creators' BAYC
+            memberships.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Distribution;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@
 export { default as Welcome } from "./welcome/Welcome.jsx";
 export { default as Header } from "./header/Header.jsx";
 export { default as Navbar } from "./navbar/Navbar.jsx";
+export { default as Distribution } from "./distribution/Distribution.jsx";
