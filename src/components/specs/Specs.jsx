e1593d53a0137a59b7eb81bf68412b92b33b2093
Dhaesha M <dhaesha1@gmail.com>
2022-03-17 10:42:48 -0400

    working on specs gif

--- src/components/specs/Specs.jsx
+++ src/components/specs/Specs.jsx
@@ -1,33 +1,17 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./specs.css";
-import ape4 from "../../assets/ape4.png";
+// import "react-gif-player/src/GifPlayer";
+
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+var GifPlayer = require("react-gif-player");
 
 const Specs = () => {
+  //   var React = require("react");
+  //   var ReactDOM = require("react-dom");
+  //   var GifPlayer = require("react-gif-player");
   return (
-    // <div className="bayc__specs">
-    //   <div className="bayc__specs-feature_container">
-    //     <h1>THE SPECS</h1>
-    //     <div className="bayc__specs-feature_paragraph-wrapper">
-    //       <p>
-    //         Each Bored Ape is unique and programmatically generated from over
-    //         170 possible traits, including expression, headwear, clothing, and
-    //         more. All apes are dope, but some are rarer than others.
-    //       </p>
-    //       <p>
-    //         The apes are stored as ERC-721 tokens on the Ethereum blockchain and
-    //         hosted on IPFS. (See Record and Proof.) Purchasing an ape costs 0.08
-    //         ETH.
-    //       </p>
-    //       <p>
-    //         To access members-only areas such as THE BATHROOM, Apeholders will
-    //         need to be signed into their Metamask Wallet.
-    //       </p>
-    //     </div>
-    //   </div>
-    //   <div className="bayc__specs-img_wrapper">
-    //     <img />
-    //   </div>
-    // </div>
     <div className="bayc__specs">
       <div className="bayc__specs-feature_container">
         <div className="bayc__specs-feature_title">
@@ -52,9 +36,13 @@ const Specs = () => {
           </p>
         </div>
       </div>
-      <div className="bayc__specs-img_container">
-        <div className="bayc__specs-img_container_inner">
-          <img src={ape4} alt="ape4" />
+      <div className="bayc__specs-gif_container">
+        <div className="bayc__specs-gif_container_inner">
+          {/* <img className="bayc__specs-gif" id="apeGif" src={ape4} alt="ape4" /> */}
+          <GifPlayer
+            gif="../../assets/mystery-ape.gif"
+            still="../../assets/ape4.png"
+          />
         </div>
       </div>
     </div>
