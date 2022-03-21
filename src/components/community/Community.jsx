46b10a55982c6a233018676acec55bddb88cde27
paintincode <93278553+paintincode@users.noreply.github.com>
2022-03-21 12:11:11 -0400

    roadmap jsx complete

--- src/components/community/Community.jsx
+++ src/components/community/Community.jsx
@@ -2,7 +2,31 @@ import React from "react";
 import "./community.css";
 
 const Community = () => {
-  return <div>Community</div>;
+  return (
+    <div className="bayc__comm">
+      <div className="bayc__comm-feature_container">
+        <div className="bayc__comm-feature_title">
+          <h1>ROADMAP ACTIVATIONS</h1>
+        </div>
+        <div className="bayc__comm-feature-text">
+          <p>
+            Here are some helpful tools created by the Bored Ape Yacht Club
+            community. Please note that these are unofficial in nature. Every
+            assignment of an ape's overall value or rarity is inherently
+            subjective.
+          </p>
+        </div>
+      </div>
+      <div className="bayc__comm-button_container">
+        <button className="bayc__comm-button1" type="button">
+          BUY AN APE ON OPENSEA
+        </button>
+        <button className="bayc__comm-button2" type="button">
+          BUY AN APE ON OPENSEA
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Community;
