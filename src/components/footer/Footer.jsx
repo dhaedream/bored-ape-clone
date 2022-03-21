3d98e8c48f69c35d1dd938b25a5d5c518c2b4d29
paintincode <93278553+paintincode@users.noreply.github.com>
2022-03-21 17:03:59 -0400

    save

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,15 @@ import "./footer.css"
 const Footer = () => {
   return (
     <div className="bayc__footer">
-      <div className="bayc__footer-list_container"></div>
+      <div className="bayc__footer-form_container">
+        <form>
+          <label>
+            <p>GET ON THE LIST</p>
+            <input type="text" name="name" value="Email Address" />
+          </label>
+          <input type="submit" value="->" />
+        </form>
+      </div>
       <div className="bayc__footer-logo_container"></div>
       <div className="bayc__footer-links_container"></div>
     </div>
